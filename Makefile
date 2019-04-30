87fc09fef9050147f6e43a689d0254f49a58f0bf
d10 <d10.luzko@gmail.com>
2019-04-30 11:51:46 +0300

    eslint

--- Makefile
+++ Makefile
@@ -1,7 +1,8 @@
 install:
 			    npm install
-
 start:
 				npx babel-node src/bin/brain-games.js
 publish:
 				npm publish
+lint:
+			npx eslint
--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import userName from '..';
-console.log ('Welcome to the Brain Games');
+
+console.log('Welcome to the Brain Games');
 
 userName();
